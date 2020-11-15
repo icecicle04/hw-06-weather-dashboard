eeae0765b7da6b5d42319f7ab1451d8e8cdd0273
AK <icecicle04@gmail.com>
2020-11-15 01:29:36 -0500

    added search button functionality and local storage features

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ $(document).ready(function () {
   var searchBtn = $("#searchButton");
   var mainForecastEl = "#mainForecast";
   var subForecastEl = "#fiveDayForecast";
+  var searchedCity = [];
 
   // searchBtn.on("click", function () {
   //     search
+
+  searchBtn.on("click", function () {
+    //initialize divs
+    $("#mainForecast").empty();
+    $("#fiveDayForecast").empty();
+    //save to local storage
+    searchedCity.push(searchInputEl.val());
+    localStorage.setItem("city", JSON.stringify(searchedCity));
+    $("city-list").empty();
+
+    //get local storage information
+    var loadedCity = JSON.parse(localStorage.getItem("city"));
+    for (var i = 0; i < loadedCity.length; i++) {
+      var searchCityBtn = $("<button>");
+      searchCityBtn.text(loadedCity[i]);
+      $("city-list").prepend(searchCityBtn);
+    }
+  });
 
   for (var i = 0; i < 10; i++) {
     var emptyEl = $("<div>");
